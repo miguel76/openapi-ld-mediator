123ef42c193efe8230abb1cdee0c2c43f09a4149
miguel76 <miguel.ceriani@gmail.com>
2017-08-07 18:20:13 +0100

    Add JSON-LD contexts

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
+const fs = require('fs');
 const Swagger = require('swagger-client');
-const jamendoApiSpec = require('./test/openapi/jamendo.json');
+const jsonld = require('jsonld');
+const Promise = require('promise');
+const _ = require('lodash');
+const async = require('async');
 
-Swagger({spec: jamendoApiSpec})
-  .then( client => {
-      console.log(client.spec); // The resolved spec
-      console.log(client.originalSpec); // In case you need it
-      console.log(client.errors); // Any resolver errors
+const readJson = fileName => cb =>
+  fs.readFile(fileName, 'utf8', (err, str) => err ? cb(err) : cb(null, JSON.parse(str)));
 
-      // Tags interface
-      //client.apis.pet.addPet({id: 1, name: "bobby"}).then(...)
+const selectAs = (inPath, outPath) => (value, obj, cb) => {
+  if (!cb) {
+    cb = obj;
+    obj = {};
+  };
+  var input = inPath ? _.get(value, inPath) : value;
+  return cb(null, outPath ? _.set(obj, outPath, input) : input);
+}
+const invokation = path => (obj, ...args) => _.invoke(obj, path, ...args);
+const dePromisify = promiseReturningFunction => (...params) => {
+    var cb = params.pop();
+    _.spread(promiseReturningFunction)(params)
+        .then(result => cb(null, result))
+        .catch(error => cb(error));
+};
 
-      // TryItOut Executor, with the `spec` already provided
-      // client.execute({operationId: 'addPet', parameters: {id: 1, name: "bobby") }).then(...)
-   });
+async.autoInject({
+  jamendoApiSpec: readJson('./test/openapi/jamendo.json'),
+  jamendoJsonLdContexts: readJson('./test/jsonld-contexts/jamendo-searchTracks-response.jsonld'),
+  jamendoSwaggerConf: ['jamendoApiSpec', selectAs(null, 'spec')],
+  jamendoApi: ['jamendoSwaggerConf', dePromisify(Swagger)],
+  jamendoSearchCall: [async.constant({
+    securities:
+        process.env.JAMENDO_CLIENT_ID ?
+            {authorized: {apikey_auth: process.env.JAMENDO_CLIENT_ID}} : {},
+    operationId: 'searchTracks',
+    parameters: {search: "punk"}
+  })],
+  jamendoApiSpecAndSearchCall: ['jamendoApiSpec', 'jamendoSearchCall', selectAs(null, 'spec')],
+  jamendoFullRequest: ['jamendoApiSpecAndSearchCall', async.asyncify(Swagger.buildRequest)],
+  jamendoSearchExecuted: ['jamendoApi', 'jamendoSearchCall', dePromisify(invokation('execute'))],
+  jamendoSearchResults: ['jamendoSearchExecuted', selectAs('body')],
+  jamendoSearchResultsLd: (jamendoSearchResults, jamendoJsonLdContexts, cb) => {
+    async.seq(
+        _.partial(async.map,
+            jamendoJsonLdContexts,
+            async.seq(
+                selectAs(null, 'expandContext'),
+                _.partial(jsonld.promises.expand, jamendoSearchResults))),
+        async.asyncify(_.flatten),
+        async.asyncify(_.spread(_.partial(_.merge, {}))))(cb);
+  },
+  result: ['jamendoSearchResultsLd', async.asyncify(_.identity)]
+}, function(err, results) {
+  if (err) {
+    console.error(err);
+  }
+  console.log(results.result);
+});
